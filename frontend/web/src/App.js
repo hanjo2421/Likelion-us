66790889f88aee92c829cd16669d67707597d748
hanjo2421 <hanjo2421@berkeley.edu>
2025-01-31 00:40:28 -0800

    Initialize React project for Mentoring Platform

---
+++ frontend/web/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20%' }}>
+      <h1>Welcome to LIKELION US</h1>
+      <p>React 프로젝트가 정상적으로 실행되었습니다.</p>
+    </div>
+  );
+}
+
+export default App;
